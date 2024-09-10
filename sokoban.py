bd735150b154f16f303c1d7bfdf3dcabcdd78dda
Jonas Nim Røssum <1959615+joglr@users.noreply.github.com>
2024-09-10 13:11:04 +0200

    Add sokoban

---
+++ sokoban.py
@@ -0,0 +1,146 @@
+import sys
+
+WALL = "X"
+AIR = " "
+CAN = "$"
+GOAL = "."
+ROBOT = "@"
+CAN_ON_GOAL = "*"
+
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+UP = (0, -1)
+DOWN = (0, 1)
+
+def read_world_and_find_pois(filename):
+    robot_pos = None
+    goal_pos = None
+    can_pos = None
+
+    with open(filename) as f:
+        world = []
+        y = 0
+        for line in f:
+            row = list(line.strip())
+            if ROBOT in row:
+                robot_pos = (row.index(ROBOT), len(world))
+
+            if GOAL in row:
+                goal_pos = (row.index(GOAL), len(world))
+
+            if CAN in row:
+                can_pos = (row.index(CAN), len(world))
+
+            world.append(row)
+            y += 1
+        return world, robot_pos, goal_pos, can_pos
+
+# Moving the robot
+
+# Tree search of moves that the cans can make
+# Weights: Distance the robot needs to move to push the box in a given direction
+
+# rx, ry = robot
+
+def bfs(world, can, goal):
+    cx, cy = can
+    tx, ty = goal
+
+    visited = set([can])
+    parent_map = {}
+    queue = [can]
+
+
+    # Queue the movements that the cans can make
+
+    i = 0
+    while len(queue) > 0:
+        pos = queue.pop(0)
+        print(f"{pos = }")
+        print(f"{queue = }")
+
+        if pos == goal:
+            print("🎖️ found goal")
+            # Return backtracked list of pushes that neees to be performed
+            path = [pos]
+            current_pos = pos
+            while current_pos != can:
+                path.append(parent_map[current_pos])
+            print("reversd_path:", path)
+            return reversed(path)
+
+
+        left = add(pos, LEFT)
+        right = add(pos, RIGHT)
+        up = add(pos, UP)
+        down = add(pos, DOWN)
+
+        print(f"{left = }")
+        print(f"{right = }")
+        print(f"{up = }")
+        print(f"{down = }")
+
+        if is_free(world, left) and is_free(world, right) and left not in visited:
+            print("can be pushed left from " + str(pos))
+            parent_map[left] = pos
+            visited.add(left)
+            queue.append(left)
+
+        if is_free(world, right) and is_free(world, left) and right not in visited:
+            print("can be pushed right from " + str(pos))
+            parent_map[right] = pos
+            visited.add(right)
+            queue.append(right)
+
+        if is_free(world, up) and is_free(world, down) and up not in visited:
+            print("can be pushed up from " + str(pos))
+            parent_map[up] = pos
+            visited.add(up)
+            queue.append(up)
+
+        if is_free(world, down) and is_free(world, up) and down not in visited:
+            print("can be pushed down from " + str(pos))
+            parent_map[down] = pos
+            visited.add(down)
+            queue.append(down)
+
+        print_world(world, queue)
+        i += 1
+
+def print_world(world, visited):
+    print()
+    for y, row in enumerate(world):
+        for x, spot in enumerate(row):
+            if (x, y) in visited:
+                print("V ", end="")
+            else:
+                print(spot + " ", end="")
+        print()
+    print()
+
+def is_free(world, pos):
+    x, y = pos
+
+    spot = None
+
+    try:
+        spot = world[y][x]
+    except IndexError:
+        return False
+
+    spot_is_free = (spot == AIR or spot == ROBOT)
+
+    return  spot_is_free
+
+def add(a, b):
+    return (a[0] + b[0], a[1] + b[1])
+
+def main():
+    filename = sys.argv[1]
+    world, robot_pos, goal_pos, can_pos = read_world_and_find_pois(filename)
+
+    path = bfs(world, can_pos, goal_pos)
+    print("path:", path)
+
+if __name__ == "__main__":
+    main()
